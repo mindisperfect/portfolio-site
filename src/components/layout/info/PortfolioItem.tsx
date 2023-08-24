2327705de565d9459ca0c1c4db2c7e05cf9cb4fc
mindisperfect <shermatovshahzod9@gmail.com>
2023-08-24 06:13:10 +0500

    jkfner

--- src/components/layout/info/PortfolioItem.tsx
+++ src/components/layout/info/PortfolioItem.tsx
@@ -57,3 +57,7 @@ const PortfolioItem = () => {
 };
 
 export default PortfolioItem;
+
+
+
+
--- src/pages/user/home/Home.tsx
+++ src/pages/user/home/Home.tsx
@@ -1,32 +1,31 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Profile from "../../../assets/images/home.jpg";
 import { FaArrowRight } from "react-icons/fa";
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
   return (
     <section className="home section grid">
-      <img
-        src={Profile}
-        alt=""
-        className="home__img"
-      />
+      <img src={Profile} alt="" className="home__img" />
       <div className="home__content">
         <div className="home__data">
           <h1 className="home__title">
-            <span>I'am Shakhzod</span> Web chala designer
+            <span>I'am Shakhzod</span> Web little Developer
           </h1>
           <p className="home__desription">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe sunt, eveniet placeat ipsam nisi animi totam repudiandae obcaecati vel officiis!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
+            sunt, eveniet placeat ipsam nisi animi totam repudiandae obcaecati
+            vel officiis!
           </p>
           <Link to="/about" className="button">
-            More About <span className="button__icon"><FaArrowRight /></span>
+            More About{" "}
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
           </Link>
         </div>
       </div>
-
-      <div className="color__block">
-      </div>
+      <div className="color__block"></div>
     </section>
   );
 };
